641bd757d533c7041d15944a18508cb78db402c7
Luke Oliver <luke@indulgemedia.com>
2025-07-10 17:16:17 +0100

    v0.3.2
    - Fix issue with not being able to scroll in tabs using arrow keys

--- resources/js/components/ui/StyledTab.vue
+++ resources/js/components/ui/StyledTab.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div class="bg-card text-card-foreground rounded-lg border shadow-sm grid h-full">
-        <div class="grid overflow-x-auto contain-size">
+        <div class="grid overflow-x-auto contain-size" tabindex="0">
             <slot />
         </div>
     </div>
